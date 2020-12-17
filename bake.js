2631cd83a485bf4961e1de5f432c1909abae7bfc
john89521 <john89521@gmail.com>
2020-12-17 09:24:05 -0800

    first commit

---
+++ bake.js
@@ -0,0 +1,18 @@
+function bake(degrees) {
+	var message;
+	
+	if (degrees > 500) {
+		message = "I'm not nuclear reactor!";
+	} else if (degrees < 100) {
+		message = "I'm not a refridgerator";
+	} else {
+		message = "That's a very confortable temperature for me.";
+		setMode("bake");
+		setTemp(degrees);
+	}
+	return message;
+}
+
+var status = bake(350);
+
+
---
+++ conditional_samples.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+if (inStock == true && (onSale == true || price < 60)) {
+    alert("buy buy buy!"); //sounds like a bargain!
+}
